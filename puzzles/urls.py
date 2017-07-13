3f32fe431d3679f3a55f51ed1efa618858b33f9b
Marc Schwartz <mathemaat@hotmail.com>
2017-07-13 21:20:02 +0200

    Add regexes for puzzle pages

--- puzzles/urls.py
+++ puzzles/urls.py
@@ -3,6 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+  # /puzzles
   url(r'^$', views.index, name='index'),
+  # /puzzles/insert/
+  url(r'^insert/$', views.insert, name='insert'),
+  # /puzzles/8/edit/
+  url(r'^(?P<puzzleId>[0-9]+)/edit/$', views.edit, name='edit'),
+  # /puzzles/8/view/
+  url(r'^(?P<puzzleId>[0-9]+)/view/$', views.view, name='view'),
 ]
 
--- puzzles/views.py
+++ puzzles/views.py
@@ -7,3 +7,12 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse("Hello, world. You're at the puzzles index.")
 
+def insert(request):
+  return HttpResponse("You're inserting a puzzle")
+
+def edit(request, puzzleId):
+  return HttpResponse("You're editing puzzle %s" % puzzleId)
+
+def view(request, puzzleId):
+  return HttpResponse("You're viewing puzzle %s" % puzzleId)
+
