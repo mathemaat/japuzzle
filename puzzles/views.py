6bff4cfa52cef7caa651f784b0364f43a6467136
Marc Schwartz <mathemaat@hotmail.com>
2017-07-13 21:48:42 +0200

    Use the render shortcut for using a template

--- puzzles/views.py
+++ puzzles/views.py
@@ -2,18 +2,17 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Puzzle
 
 # Create your views here.
 def index(request):
   puzzles = Puzzle.objects.all()
-  template = loader.get_template('puzzles/index.html')
   context = {
     'puzzles': puzzles
   }
-  return HttpResponse(template.render(context, request))
+  return render(request, 'puzzles/index.html', context)
 
 def insert(request):
   return HttpResponse("You're inserting a puzzle")
